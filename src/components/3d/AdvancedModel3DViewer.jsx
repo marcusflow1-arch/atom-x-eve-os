0f2af646da1c38a07a6b6448cf1831c3e7e21564
marcus flowers <marcusflow7@gmail.com>
2025-12-12 11:37:38 +0000

    File changes

--- src/components/3d/AdvancedModel3DViewer.jsx
+++ src/components/3d/AdvancedModel3DViewer.jsx
@@ -6,12 +6,6 @@ import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
-import { SSAOPass } from 'three/examples/jsm/postprocessing/SSAOPass';
-import { SMAAPass } from 'three/examples/jsm/postprocessing/SMAAPass';
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
-import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader';
-import { ColorCorrectionShader } from 'three/examples/jsm/shaders/ColorCorrectionShader';
-import { VignetteShader } from 'three/examples/jsm/shaders/VignetteShader';
 
 export default function AdvancedModel3DViewer({ modelUrl }) {
   const canvasRef = useRef(null);
