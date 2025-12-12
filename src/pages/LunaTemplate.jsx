3e5bc61678524c96a4dc7e0db63f52a4cd3ae31e
marcus flowers <marcusflow7@gmail.com>
2025-12-12 02:21:11 +0000

    File changes

--- src/pages/LunaTemplate.jsx
+++ src/pages/LunaTemplate.jsx
@@ -740,14 +740,14 @@ export default function LunaTemplate() {
                 />
               </div>
 
-              {/* 3D Model in Center */}
-              {uiVisible && !showInventory && !expandedGenre && (
+              {/* 3D Model in Center - Temporarily Disabled Due to GLB Loading Error */}
+              {/* {uiVisible && !showInventory && !expandedGenre && (
                 <div className="fixed inset-0 flex items-center justify-center pointer-events-none z-10">
                   <div className="w-[600px] h-[600px] pointer-events-auto">
                     <SinesModel />
                   </div>
                 </div>
-              )}
+              )} */}
 
               {/* Main Content Area */}
               <div className="w-full mt-24 px-12 relative">
