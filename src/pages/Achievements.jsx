335fbb94cb965a8bfb51288caf11568f1442b40c
marcus flowers <marcusflow7@gmail.com>
2025-12-21 07:11:32 +0000

    File changes

--- src/pages/Achievements.jsx
+++ src/pages/Achievements.jsx
@@ -15,6 +15,8 @@ import ChallengeFriendModal from '../components/community/ChallengeFriendModal';
 import CardEnhancementOverlay from '../components/profile/CardEnhancementOverlay';
 import ShinyCard from '../components/shared/ShinyCard';
 import { base44 } from '@/api/base44Client';
+import { Link } from 'react-router-dom';
+import { createPageUrl } from '@/utils';
 
 // --- Shiny Sidebar Box Component ---
 const ShinySidebarBox = ({ children, className = "" }) => {
