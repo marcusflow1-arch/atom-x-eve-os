3a301aa1f4da7ed6c6d1aadaef9bf9df23e4f6f5
marcus flowers <marcusflow7@gmail.com>
2025-12-21 08:20:23 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -605,9 +605,9 @@ function LayoutContent({ children, currentPageName }) {
                 <AnimatePresence mode="wait">
                   <motion.div
                     key={location.pathname}
-                    initial={{ opacity: 0, y: 8 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0, y: -8 }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
                   >
                     {children}
                   </motion.div>
