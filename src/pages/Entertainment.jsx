4d210b4f7e19122f7c8a1651152bb635f70b711d
marcus flowers <marcusflow7@gmail.com>
2025-12-21 13:59:52 +0000

    File changes

--- src/pages/Entertainment.jsx
+++ src/pages/Entertainment.jsx
@@ -3,7 +3,7 @@ import EntertainmentUI from "@/components/entertainment/EntertainmentUI";
 
 export default function Entertainment() {
   return (
-    <div className="min-h-screen w-full pt-20">
+    <div className="min-h-screen w-full pt-0">
       <EntertainmentUI />
     </div>
   );
