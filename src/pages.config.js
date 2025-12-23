b9cf7950d48b3bf974c31b8f16454bdf62ca44b2
marcus flowers <marcusflow7@gmail.com>
2025-12-23 21:01:45 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,7 +1,6 @@
 import AIConsole from './pages/AIConsole';
 import AINews from './pages/AINews';
 import AbilityAchievements from './pages/AbilityAchievements';
-import Achievements from './pages/Achievements';
 import AdamXEve from './pages/AdamXEve';
 import Admin from './pages/Admin';
 import AvatarStudio from './pages/AvatarStudio';
@@ -20,7 +19,6 @@ import GenreMastery from './pages/GenreMastery';
 import Home from './pages/Home';
 import Ideals from './pages/Ideals';
 import InstallApp from './pages/InstallApp';
-import Library from './pages/Library';
 import LunaTemplate from './pages/LunaTemplate';
 import Mailbox from './pages/Mailbox';
 import Notifications from './pages/Notifications';
@@ -37,6 +35,8 @@ import StreamerProfile from './pages/StreamerProfile';
 import StreamerProfileEdit from './pages/StreamerProfileEdit';
 import StreamingHub from './pages/StreamingHub';
 import WorldEvents from './pages/WorldEvents';
+import Library from './pages/Library';
+import Achievements from './pages/Achievements';
 import __Layout from './Layout.jsx';
 
 
@@ -44,7 +44,6 @@ export const PAGES = {
     "AIConsole": AIConsole,
     "AINews": AINews,
     "AbilityAchievements": AbilityAchievements,
-    "Achievements": Achievements,
     "AdamXEve": AdamXEve,
     "Admin": Admin,
     "AvatarStudio": AvatarStudio,
@@ -63,7 +62,6 @@ export const PAGES = {
     "Home": Home,
     "Ideals": Ideals,
     "InstallApp": InstallApp,
-    "Library": Library,
     "LunaTemplate": LunaTemplate,
     "Mailbox": Mailbox,
     "Notifications": Notifications,
@@ -80,6 +78,8 @@ export const PAGES = {
     "StreamerProfileEdit": StreamerProfileEdit,
     "StreamingHub": StreamingHub,
     "WorldEvents": WorldEvents,
+    "Library": Library,
+    "Achievements": Achievements,
 }
 
 export const pagesConfig = {
