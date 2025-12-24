30efe725afa3d91689f5dceebede87112163d182
marcus flowers <marcusflow7@gmail.com>
2025-12-24 00:36:13 +0000

    File changes

--- src/pages/Achievements.jsx
+++ src/pages/Achievements.jsx
@@ -776,17 +776,6 @@ function AchievementsView({ onExitToLibrary }) {
                 </Link>
               )}
               
-              {/* Controller Icon */}
-              <Link to={createPageUrl('Library')} title="Go to Library">
-                <motion.button
-                  whileHover={{ scale: 1.1 }}
-                  whileTap={{ scale: 0.95 }}
-                  className="w-8 h-8 rounded-lg bg-white/10 hover:bg-white/20 flex items-center justify-center border border-white/15"
-                >
-                  <Gamepad2 className="w-5 h-5 text-white/80" />
-                </motion.button>
-              </Link>
-              
               {/* Grid Toggle Button */}
               <motion.button
                 onClick={() => setViewMode(viewMode === 'list' ? 'grid' : 'list')}
