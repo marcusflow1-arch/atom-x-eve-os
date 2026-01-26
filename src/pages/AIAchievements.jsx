7d1ec3b467d30c82d9e7d0e0fee63fdd3703a018
marcus flowers <marcusflow7@gmail.com>
2026-01-26 18:05:12 +0000

    File changes

--- src/pages/AIAchievements.jsx
+++ src/pages/AIAchievements.jsx
@@ -475,12 +475,7 @@ function AIAchievementsView({ onClosePage }) {
           {/* LEFT COLUMN: Library + Game Banner below */}
           <div className="w-[320px] flex-shrink-0 h-full flex flex-col gap-4">
             
-            {/* Header */}
-            <div className="flex items-center gap-3">
-              <h1 className="text-2xl font-black tracking-tighter text-white">
-                Library
-              </h1>
-            </div>
+
 
             {/* Library Section - Now at top, takes most space */}
             <ShinySidebarBox className="flex-1 flex flex-col p-5 min-h-0">
