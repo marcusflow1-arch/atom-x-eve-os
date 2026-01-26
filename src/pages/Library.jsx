74c429cc765e5becc467c9c5fe4c9ac34a9d33e8
marcus flowers <marcusflow7@gmail.com>
2026-01-26 18:10:05 +0000

    File changes

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1284,7 +1284,7 @@ export default function Library({ onSwitchToStore, onSwitchToAchievements }) {
               </div>
             </button>
             <h1 className="text-2xl font-bold text-white tracking-wide">Atom X Eve Library</h1>
-            <div className="h-6 w-px bg-white/10" />
+            <div className="h-8 w-px bg-white/10" />
 
             {/* Achievements Link */}
             <button
