cdd953a4d761cd878ef75d2b3b14793f25674892
marcus flowers <marcusflow7@gmail.com>
2026-01-27 03:36:28 +0000

    File changes

--- src/pages/Streaming.jsx
+++ src/pages/Streaming.jsx
@@ -4,9 +4,7 @@ import StreamingGamesLive from '../components/streaming/StreamingGamesLive';
 export default function Streaming() {
   return (
     <div className="w-full h-full min-h-screen">
-      <div className="max-w-[1920px] mx-auto px-4 text-white/70 py-24">
-        <p>Loading...</p>
-      </div>
+      <StreamingGamesLive />
     </div>
   );
 }
