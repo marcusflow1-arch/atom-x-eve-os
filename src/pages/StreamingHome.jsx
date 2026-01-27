0f55cb10a437f49bd6e49f83b935cdf81873f76e
marcus flowers <marcusflow7@gmail.com>
2026-01-27 05:42:33 +0000

    File changes

--- src/pages/StreamingHome.jsx
+++ src/pages/StreamingHome.jsx
@@ -727,7 +727,6 @@ export default function StreamingHome() {
                                         </div>
                                     </DialogContent>
                                 </Dialog>
-                                </div>
                             </div>
                         )}
                     </motion.div>
