c33e110280bda08506624168a1e5981875f63ced
marcus flowers <marcusflow7@gmail.com>
2026-01-27 03:41:18 +0000

    File changes

--- src/pages/Streaming.jsx
+++ src/pages/Streaming.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import StreamingGamesLive from '../components/streaming/StreamingGamesLive';
+import BottomQuickBar from '../components/streaming/BottomQuickBar';
 
 export default function Streaming() {
   return (
     <div className="w-full h-full min-h-screen">
       <StreamingGamesLive />
+      <BottomQuickBar />
     </div>
   );
 }
