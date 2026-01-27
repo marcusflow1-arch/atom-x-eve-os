1855eee9d4bfe8fc948a32eae51c839b4ab32351
marcus flowers <marcusflow7@gmail.com>
2026-01-27 03:35:29 +0000

    File changes

--- src/pages/Streaming.jsx
+++ src/pages/Streaming.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default function Streaming() {
   return (
-    <div className="w-full h-screen" />
+    <div className="w-full h-full min-h-screen">
+      <div className="max-w-[1920px] mx-auto px-4 text-white/70 py-24">
+        <p>Loading...</p>
+      </div>
+    </div>
   );
 }
