1a6eeac84f79a93743371ab2419a5cb74d06bda7
marcus flowers <marcusflow7@gmail.com>
2026-01-29 01:10:34 +0000

    File changes

--- src/components/shared/ShinyCard.jsx
+++ src/components/shared/ShinyCard.jsx
@@ -34,7 +34,7 @@ export default function ShinyCard({ children, onClick, className = "" }) {
       style={{ 
         rotateX, 
         rotateY,
-        scale: 0.575,
+        scale: 1,
         transformStyle: "preserve-3d",
       }}
       className={`relative rounded-xl overflow-hidden cursor-pointer group ${className}`}
