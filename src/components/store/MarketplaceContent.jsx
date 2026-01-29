67050d6f562287834ca1df51e96f7e3ae3a9836f
marcus flowers <marcusflow7@gmail.com>
2026-01-29 09:40:52 +0000

    File changes

--- src/components/store/MarketplaceContent.jsx
+++ src/components/store/MarketplaceContent.jsx
@@ -17,6 +17,7 @@ import { useNavigate } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { Slider } from '@/components/ui/slider';
 import { Dialog, DialogContent } from '@/components/ui/dialog';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import ShinyCard from '@/components/shared/ShinyCard';
 import DeveloperLimitedEdition from './DeveloperLimitedEdition';
 
