82a7fdd5b3b82c067cfe1d11989fd50c7538337f
marcus flowers <marcusflow7@gmail.com>
2026-01-31 08:19:21 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -57,7 +57,6 @@ import Achievements from './pages/Achievements';
 import AdamXEve from './pages/AdamXEve';
 import Admin from './pages/Admin';
 import AdminUIBuilder from './pages/AdminUIBuilder';
-import Aura from './pages/Aura';
 import AvatarHome from './pages/AvatarHome';
 import AvatarStudio from './pages/AvatarStudio';
 import Blacksmith from './pages/Blacksmith';
@@ -119,7 +118,6 @@ export const PAGES = {
     "AdamXEve": AdamXEve,
     "Admin": Admin,
     "AdminUIBuilder": AdminUIBuilder,
-    "Aura": Aura,
     "AvatarHome": AvatarHome,
     "AvatarStudio": AvatarStudio,
     "Blacksmith": Blacksmith,
