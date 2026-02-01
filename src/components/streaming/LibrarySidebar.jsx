ece4a7d937f4d6a07308d7e106269d26ca766e7a
marcus flowers <marcusflow7@gmail.com>
2026-02-01 05:15:45 +0000

    File changes

--- src/components/streaming/LibrarySidebar.jsx
+++ src/components/streaming/LibrarySidebar.jsx
@@ -410,7 +410,7 @@ export default function LibrarySidebar() {
                     </button>
                 </div>
 
-                <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+                <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2">
                     {libraryGames.map((game, i) => (
                         <motion.div
                             key={`full_lib_${game.id || i}`}
