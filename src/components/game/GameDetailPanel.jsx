5827b0531b5fd503cdcc9347aeceae40c7282cd1
marcus flowers <marcusflow7@gmail.com>
2026-02-07 19:57:55 +0000

    File changes

--- src/components/game/GameDetailPanel.jsx
+++ src/components/game/GameDetailPanel.jsx
@@ -859,12 +859,6 @@ export default function GameDetailPanel({ gameId, onClose }) {
                     ))}
                   </div>
 
-                  {/* Achievement Card Strip - Below media */}
-                  <AchievementCardStrip 
-                    achievementCards={achievementCards} 
-                    dlcList={dlcList}
-                    onSelectCard={setSelectedCard} 
-                  />
                 </div>
 
                 {/* Right: Game Info Sidebar */}
