e3686afa167db5e4249582250ca6c57eb276431f
marcus flowers <marcusflow7@gmail.com>
2026-02-09 07:05:32 +0000

    File changes

--- src/components/streaming/LibrarySidebar.jsx
+++ src/components/streaming/LibrarySidebar.jsx
@@ -8,6 +8,7 @@ import QuickInfoOverlay from '@/components/streaming/QuickInfoOverlay';
 import { playItem } from '@/functions/playItem';
 import { libraryGames } from '../dashboard/gamehub/mockLibraryData';
 import InventoryFullPanel, { InventoryItemDetailPanel } from './inventory/InventoryFullPanel';
+import RewardActionPanel from './inventory/RewardActionPanel';
 
 export default function LibrarySidebar() {
   const [isOpen, setIsOpen] = useState(false);
