804b3b724afeb776d9fae6f84a036d0f688108b7
marcus flowers <marcusflow7@gmail.com>
2026-02-13 06:41:00 +0000

    File changes

--- src/components/dashboard/FocusModePanel.jsx
+++ src/components/dashboard/FocusModePanel.jsx
@@ -1739,16 +1739,14 @@ export default function FocusModePanel({ onBackgroundChange, onOpenCalendar, onT
               />
             </div>
 
-            {/* Card Collection Browser - centered above Developer Spotlight */}
-            <div className="pointer-events-auto flex justify-center" style={{ marginTop: 'auto', paddingTop: '24px' }}>
-              <div className="w-[280px]">
+            {/* Card Collection + Developer Spotlight side by side */}
+            <div className="pointer-events-auto flex items-end gap-4" style={{ marginTop: 'auto', paddingTop: '24px' }}>
+              <div className="w-[280px] flex-shrink-0">
                 <CardCollectionBrowser />
               </div>
-            </div>
-
-            {/* Developer Spotlight */}
-            <div className="pointer-events-auto" style={{ paddingTop: '16px' }}>
-              <DevSpotlightRibbon onOpenOverlay={onOpenDevSpotlight} />
+              <div className="flex-1 min-w-0">
+                <DevSpotlightRibbon onOpenOverlay={onOpenDevSpotlight} />
+              </div>
             </div>
 
             <AnimatePresence>
