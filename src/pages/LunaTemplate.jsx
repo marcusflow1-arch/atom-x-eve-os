0544b8a050ee3cdc54f1e8cf67c28e0f686b6cde
marcus flowers <marcusflow7@gmail.com>
2026-02-16 14:34:55 +0000

    File changes

--- src/pages/LunaTemplate.jsx
+++ src/pages/LunaTemplate.jsx
@@ -1043,7 +1043,7 @@ function TransparentModel3DViewer({ modelUrl, weaponModel, triggerAnimation, bac
       if (modelRef.current?.userData?._hurricaneCleanup) modelRef.current.userData._hurricaneCleanup();
       renderer.dispose();
     };
-  }, [adminAnimations, keybinds]);
+  }, [adminAnimations, keybinds, spawnableAIModels]);
 
   return (
     <div ref={containerRef} className="w-full h-full relative">
