5ea802afbcccfe93635d4896b92ffb9ca173635d
marcus flowers <marcusflow7@gmail.com>
2026-02-18 09:29:46 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1008,6 +1008,10 @@ function LayoutContent({ children, currentPageName }) {
                             <Radio className="w-4 h-4" /> Aura
                           </button>
 
+                          <button onClick={() => navigate(createPageUrl('Engine'))} className={achBtnClass(ap.includes('/engine'))}>
+                            <Rocket className="w-4 h-4" /> Engine
+                          </button>
+
                           <a
                             href="https://discord.gg/awFvGxC5"
                             target="_blank"
