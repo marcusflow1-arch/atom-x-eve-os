50ad8d5567d4f2be3bd0deb1e553027da82a0ad3
marcus flowers <marcusflow7@gmail.com>
2026-03-12 10:47:15 +0000

    File changes

--- src/components/environment/EnvironmentHub.jsx
+++ src/components/environment/EnvironmentHub.jsx
@@ -218,7 +218,7 @@ export default function EnvironmentHub({ currentEnvId, onSelectEnv, onClose, exp
                     </AnimatePresence>
                   </>
                 )}
-              </>
+              </div>
             )}
 
             {activeTab === 'skyboxes' && (
