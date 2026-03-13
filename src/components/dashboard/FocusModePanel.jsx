82b1e529b206ecdca5917cc2fd1bcd1d59a4ac2f
marcus flowers <marcusflow7@gmail.com>
2026-03-13 16:39:05 +0000

    File changes

--- src/components/dashboard/FocusModePanel.jsx
+++ src/components/dashboard/FocusModePanel.jsx
@@ -1332,7 +1332,7 @@ export function LibraryBannerSection({
             </div>
 
             {/* Calendar Box */}
-            <div className="w-[368px] h-full flex-shrink-0">
+            <div className="flex-1 min-w-0 h-full">
               {calendarBox}
             </div>
           </div>
