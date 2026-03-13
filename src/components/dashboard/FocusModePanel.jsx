922c1d7405647870e3334be94501be503d920cdc
marcus flowers <marcusflow7@gmail.com>
2026-03-13 19:50:14 +0000

    File changes

--- src/components/dashboard/FocusModePanel.jsx
+++ src/components/dashboard/FocusModePanel.jsx
@@ -1719,12 +1719,6 @@ export default function FocusModePanel({ onBackgroundChange, onOpenCalendar, onT
     setShowGamePanel(false);
   };
 
-  useEffect(() => {
-    const handleInvite = (e) => setActiveInvite(e.detail);
-    window.addEventListener('incomingInvite', handleInvite);
-    return () => window.removeEventListener('incomingInvite', handleInvite);
-  }, []);
-
   const handleDateTimeClick = () => {
     setShowUpdatesOverlay(true);
   };
