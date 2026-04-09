ed2ab076e591241aef4a02422fd5c34032083058
marcus flowers <marcusflow7@gmail.com>
2026-04-09 18:51:02 +0000

    File changes

--- src/components/shared/GlassPageFrame.jsx
+++ src/components/shared/GlassPageFrame.jsx
@@ -43,7 +43,7 @@ export default function GlassPageFrame({ children, bottomContent, topContent, cl
         }}
       >
         {bottomContent && (
-          <div className="h-full flex items-center px-6 py-2">
+          <div className="h-full w-full flex items-center justify-center px-6 py-2">
             {bottomContent}
           </div>
         )}
