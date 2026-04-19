a88518f2026b9bf1c86e9ae20c0fb6e83f657e63
marcus flowers <marcusflow7@gmail.com>
2026-04-19 21:53:27 +0000

    Refactoring GameDetail Page Layout

--- src/pages/GameDetail.jsx
+++ src/pages/GameDetail.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import GameDetailPanel from '../components/store/GameDetailPanel';
+import GameDetailPanel from '../components/game/GameDetailPanel';
 import { createPageUrl } from '@/utils';
 
 export default function GameDetail() {
@@ -17,14 +17,11 @@ export default function GameDetail() {
   };
 
   return (
-    <div
-      className="absolute inset-0 overflow-hidden"
-      style={{ top: '64px', bottom: '48px', left: '80px', right: '0' }}
-    >
-      <GameDetailPanel 
-        gameId={gameId}
-        onClose={handleClose}
-      />
-    </div>
+    <GameDetailPanel 
+      gameId={gameId} 
+      onClose={handleClose}
+      showBackButton={true}
+      from={from}
+    />
   );
 }
