58b0d0a6c6c1f8f23da2e3916c34f7d5d5039492
marcus flowers <marcusflow7@gmail.com>
2026-05-18 15:00:46 +0000

    File changes

--- src/components/game3d/hud/HUDVitalsRow.jsx
+++ src/components/game3d/hud/HUDVitalsRow.jsx
@@ -670,7 +670,7 @@ export default function HUDVitalsRow({ hp, maxHp, fusion, unspentPoints, killCou
         <div
           className="pointer-events-none absolute"
           style={{
-            right: 52 /* SkillsBookButton width */ + 8 /* gap */,
+            right: 0,
             bottom: 62 /* align with HP-side skill icons resting on gauge */,
             width: 320,
             zIndex: 3,
