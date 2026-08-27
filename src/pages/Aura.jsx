89847f323e6c36435bb4f551df9e0a9850bcd036
marcusflow1-arch <marcusflow1@gmail.com>
2026-08-27 10:30:35 -0400

    Wire Aura stream browser overlay into Aura page

--- src/pages/Aura.jsx
+++ src/pages/Aura.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import StreamingGamesLive from '@/components/streaming/StreamingGamesLive';
 import GlassPageFrame from '@/components/shared/GlassPageFrame';
 import { useSidebarVisible } from '../hooks/useSidebarVisible';
 import AuraBottomNav from '@/components/streaming/AuraBottomNav.jsx';
 import SideAccessMenu from '@/components/dashboard/SideAccessMenu';
+import AuraStreamBrowserOverlay from '@/components/streaming/AuraStreamBrowserOverlay.jsx';
 
 export default function Aura() {
-  const navigate = useNavigate();
   const [sidebarVisible, toggleSidebar] = useSidebarVisible();
 
   return (
     <GlassPageFrame sidebarVisible={sidebarVisible} onSidebarToggle={toggleSidebar} bottomContent={<AuraBottomNav />}>
-    <SideAccessMenu />
-    <div className="h-screen w-full flex relative overflow-hidden bg-[#0f1419]">
-      {/* Main Area */}
-      <div className="flex-1 relative h-full overflow-y-auto pl-6">
-        {/* Main Content */}
-        <div className="pt-20 pb-28 min-h-screen">
-          <StreamingGamesLive />
+      <SideAccessMenu />
+      <div className="h-screen w-full flex relative overflow-hidden bg-[#0f1419]">
+        <div className="flex-1 relative h-full overflow-y-auto pl-6">
+          <div className="pt-20 pb-28 min-h-screen">
+            <StreamingGamesLive />
+          </div>
         </div>
-
       </div>
-    </div>
+      <AuraStreamBrowserOverlay />
     </GlassPageFrame>
   );
 }
